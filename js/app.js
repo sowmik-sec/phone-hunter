bb63692766666521277d82b9938c3261e2235d77
sowmik-sec <sowmik.sec@gmail.com>
2023-02-05 21:01:08 +0600

    This is my mistake not to commit codes before. I'm commiting all the code at a time

---
+++ js/app.js
@@ -0,0 +1,113 @@
+const loadPhone = async (searchText, dataLimit) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  );
+  const data = await res.json();
+  displayPhones(data.data, dataLimit);
+};
+
+const displayPhones = (phones, dataLimit) => {
+  //   console.log(phones);
+  const phonesContainer = document.getElementById("phone-container");
+  phonesContainer.textContent = ``;
+  //   display 10 phones only
+  const showAll = document.getElementById("show-all");
+  if (dataLimit != undefined && phones.length > 10) {
+    console.log("here it comes");
+    phones = phones.slice(0, 10);
+    showAll.classList.remove("d-none");
+  } else {
+    showAll.classList.add("d-none");
+  }
+  console.log("phone number", phones.length);
+  //   display no phones found
+  const noFoundMessage = document.getElementById("no-found-message");
+  if (phones.length == 0) {
+    noFoundMessage.classList.remove("d-none");
+  } else {
+    noFoundMessage.classList.add("d-none");
+  }
+
+  phones.forEach((phone) => {
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("col");
+    phoneDiv.innerHTML = `
+    <div class="card p-4">
+        <img src="${phone.image}" class="card-img-top" alt="..." />
+        <div class="card-body">
+        <h5 class="card-title">${phone.phone_name}</h5>
+        <p class="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit
+            longer.
+        </p>
+        <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Go Somewhere</button>
+        </div>
+    </div>
+    `;
+    phonesContainer.appendChild(phoneDiv);
+  });
+  //   stop loader
+  toggleSpinner(false);
+};
+
+const processSearch = (dataLimit) => {
+  toggleSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  //searchField.value = "";
+  console.log("data limit", dataLimit);
+  loadPhone(searchText, dataLimit);
+};
+
+document.getElementById("btn-search").addEventListener("click", function () {
+  // start loader
+  processSearch(10);
+});
+
+// search input field enter key handler
+document
+  .getElementById("search-field")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      processSearch(10);
+    }
+  });
+
+document.getElementById("btn-show-all").addEventListener("click", function () {
+  processSearch();
+});
+
+const toggleSpinner = (isLoading) => {
+  const loaderSection = document.getElementById("loader");
+  if (isLoading) {
+    loaderSection.classList.remove("d-none");
+  } else {
+    loaderSection.classList.add("d-none");
+  }
+};
+
+const loadPhoneDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhoneDetails(data.data);
+};
+
+const displayPhoneDetails = (phone) => {
+  console.log(phone);
+  const modalTitle = document.getElementById("phoneDetailModalLabel");
+  modalTitle.innerText = phone.name;
+  const phoneDetails = document.getElementById("phone-details");
+  phoneDetails.innerHTML = `
+    <p>Release Date: ${
+      phone.releaseDate ? phone.releaseDate : "No Release Date"
+    }</p>
+    <p>Storage: ${
+      phone.mainFeatures
+        ? phone.mainFeatures.storage
+        : "No Storage Information found"
+    }</p>
+    <p>Others: ${phone.others ? phone.others.Bluetooth : "No Bluetooth"}</p>
+  `;
+};
